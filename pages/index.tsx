423e18e524cabea4895808bd19ca4b412bfa96db
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-06-22 23:36:05 +0700

    implement next-theme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,25 @@
+import { useTheme } from 'next-themes';
+
 import { Head } from '../components/Head';
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
   return (
     <>
-      {/* <Head title='Home' /> */}
-      <h1>Welcome to PAPA</h1>
+      <Head title='Home' />
+
+      <div className='flex h-screen'>
+        <div className='m-auto text-center'>
+          <h1 className='dark:text-white mb-20 text-9xl'>Welcome to PAPA</h1>
+          <button
+            className='py-2 px-10 bg-blue-500 rounded-md focus:outline-none text-white'
+            onClick={() => setTheme(theme == 'light' ? 'dark' : 'light')}
+          >
+            {theme == 'light' ? 'Dark' : 'Light'}
+          </button>
+        </div>
+      </div>
     </>
   );
 }
