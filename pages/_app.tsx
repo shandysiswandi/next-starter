37618c35caf52e7915a7c5806a990b75c31d8ee8
Shandy Siswandi <shandysiswandi@jojonomic.com>
2021-06-22 23:24:44 +0700

    inject redux store to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from 'next-themes';
+import { Provider } from 'react-redux';
+import { store } from 'store';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <ThemeProvider attribute='class'>
-        <Component {...pageProps} />
+      <ThemeProvider storageKey='next-theme' attribute='class'>
+        <Provider store={store}>
+          <Component {...pageProps} />
+        </Provider>
       </ThemeProvider>
     </>
   );
